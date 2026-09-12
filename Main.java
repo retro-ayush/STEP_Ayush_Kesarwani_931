01117d1874b51567c41ab967abbc952deb661334
retro-ayush <shivoy002@gmail.com>
2026-09-12 16:14:10 +0530

    Section 1: Part 4: remaining part refactor

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        AccountRepository repo = new AccountRepository();
+        NotificationService notifier = new NotificationService();
+        StatementGenerator statements = new StatementGenerator();
+        BankAccount account = new BankAccount(931, "Ayush", 20, 5000.0, "Savings");
+        account.setPin(1234);
+        if (account.deposit(1500)) {
+            notifier.send("Your deposit of Rs. 1500.0 was successful. New balance: " + account.getBalance());
+            repo.save(account);
+        }
+        if (account.deposit(800)) {
+            notifier.send("Your deposit of Rs. 800.0 was successful. New balance: " + account.getBalance());
+            repo.save(account);
+        }
+        if (account.withdraw(2000, 1234)) {
+            notifier.send("Your withdrawal of Rs. 2000.0 was successful. New balance: " + account.getBalance());
+            repo.save(account);
+        }
+        System.out.println(statements.generate(account));
+    }
+}
